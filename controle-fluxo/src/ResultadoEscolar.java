51ddbb9d7c4110dd86c42a195e0c1362ba224da5
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-04-30 11:55:04 -0300

    added controle-fluxo :sparkles:

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,50 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        /**
+         * <p>
+         * Condição composta
+         * </p>
+         */
+        // int nota = 6;
+
+        // if (nota >= 7)
+        // System.out.println("Aprovado");
+
+        // else
+        // System.out.println("Reprovado");
+
+        /**
+         * <p>
+         * Condição encadeada
+         * </p>
+         */
+
+        // int nota = 6;
+
+        // if (nota >= 7)
+        // System.out.println("Aprovado");
+        // else if (nota >= 5 && nota < 7)
+        // System.out.println("Recuperação");
+        // else
+        // System.out.println("Reprovado");
+        // }
+
+        /**
+         * <p>
+         * Condição ternária
+         * </p>
+         */
+        // Cenário 1
+        // int nota = 7;
+        // String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        // System.out.println(resultado);
+
+        // Cenário 2
+        // int nota = 6;
+        // String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ?
+        // "Recuperação" : "Reprovado";
+        // System.out.println(resultado);
+
+        
+    }
+}
