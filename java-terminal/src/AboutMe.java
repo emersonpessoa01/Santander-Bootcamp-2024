b13c8adca30ab25fce76ed1760db253f330b06d7
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-05-01 10:45:23 -0300

    added try/catch :rocket:

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -8,40 +8,45 @@ public class AboutMe {
      * 
      * @param args
      */
-    /**public static void main(String[] args) {
-        // os argumentos começam com indice 0
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]); // vamos falar sobre Wrappers
-        double altura = Double.valueOf(args[3]);
-
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura eh " + altura + "cm");
-    }
-    */
+    /**
+     * public static void main(String[] args) {
+     * // os argumentos começam com indice 0
+     * String nome = args[0];
+     * String sobrenome = args[1];
+     * int idade = Integer.valueOf(args[2]); // vamos falar sobre Wrappers
+     * double altura = Double.valueOf(args[3]);
+     * 
+     * System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+     * System.out.println("Tenho " + idade + " anos ");
+     * System.out.println("Minha altura eh " + altura + "cm");
+     * }
+     */
     public static void main(String[] args) {
-        //criando o objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
-        
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
+        try {
+            try (// criando o objeto scanner
+                    Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+                System.out.println("Digite seu nome");
+                String nome = scanner.next();
 
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
-        
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
+                System.out.println("Digite seu sobrenome");
+                String sobrenome = scanner.next();
 
-        
-        //imprimindo os dados obtidos pelo usuario
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
-        
-        
+                System.out.println("Digite sua idade");
+                int idade = scanner.nextInt();
+
+                System.out.println("Digite sua altura");
+                double altura = scanner.nextDouble();
+
+                // imprimindo os dados obtidos pelo usuario
+                System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+                System.out.println("Tenho " + idade + " anos ");
+                System.out.println("Minha altura é " + altura + "cm ");
+            }
+
+        } catch (Exception e) {
+            System.out.println("O campos idade e altura precisam ser numéricos");
+
+        }
+
     }
 }
