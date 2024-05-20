5a0c07f8339c8a45c9cf023d25ce731597d45f5f
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-05-20 16:40:43 -0300

    updated catalogo livros :sparkles:

--- collections-java-api-2024/src/main/java/list/pesquisa/CatalogoLivros.java
+++ collections-java-api-2024/src/main/java/list/pesquisa/CatalogoLivros.java
@@ -70,4 +70,23 @@ public class CatalogoLivros {
         }
         return livroPorTitulo;
     }
+
+    @Override
+    public String toString() {
+        return "CatalogoLivros [livroList=" + livroList + "]";
+    }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivros("Livro 1","Autor 1",2020);
+        catalogoLivros.adicionarLivros("Livro 1","Autor 2",2021);
+        catalogoLivros.adicionarLivros("Livro 2","Autor 2",2022);
+        catalogoLivros.adicionarLivros("Livro 3","Autor 3",2023);
+        catalogoLivros.adicionarLivros("Livro 4","Autor 4",1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020, 2022));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+
+    }
 }
