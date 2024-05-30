d3ef2ab6a84d349dbba5ab8092eb19218645e326
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-05-30 18:44:09 -0300

    updated folders :rocket:

--- ganhando_produtividade_com_Stream_API_Java/src/funcional_iterface/examples/ConsumerExample.java
+++ ganhando_produtividade_com_Stream_API_Java/src/funcional_iterface/examples/ConsumerExample.java
@@ -9,6 +9,15 @@ public class ConsumerExample {
     public static <T> void main(String[] args) {
         // Criar uma lista de números inteiros
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        numeros.forEach(n -> {
+            if (n % 2 == 0) {
+                System.out.println(n);
+            }
+        }
+
+        );
+        // Criar uma lista de números inteiros
+        // List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
 
         // // Método para imprimir os números
         // Consumer<Integer> imprimirNumeroPar = numero -> {
@@ -30,13 +39,7 @@ public class ConsumerExample {
         // }
 
         // });
-        numeros.forEach(n -> {
-            if (n % 2 == 0) {
-                System.out.println(n);
-            }
-        }
 
-        );
     }
 
 }
