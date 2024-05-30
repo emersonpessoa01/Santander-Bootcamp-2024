6227bf764efe01e2732709abb4daab6ce6a82534
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-05-30 18:41:44 -0300

    added consumer :rocket:

---
+++ ganhando_produtividade_com_Stream_API_Java/src/funcional_iterface/examples/ConsumerExample.java
@@ -0,0 +1,42 @@
+package funcional_iterface.examples;
+
+import java.util.Arrays;
+import java.util.List;
+// import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    public static <T> void main(String[] args) {
+        // Criar uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        // // Método para imprimir os números
+        // Consumer<Integer> imprimirNumeroPar = numero -> {
+        // if (numero % 2 == 0) {
+        // System.out.println(numero);
+        // }
+        // return;
+        // };
+
+        // numeros.stream().forEach(imprimirNumeroPar);
+
+        // Método para imprimir os números
+        // numeros.stream().forEach(new Consumer<Integer>() {
+        // @Override
+        // public void accept(Integer n) {
+        // if (n % 2 == 0) {
+        // System.out.println(n);
+        // }
+        // }
+
+        // });
+        numeros.forEach(n -> {
+            if (n % 2 == 0) {
+                System.out.println(n);
+            }
+        }
+
+        );
+    }
+
+}
