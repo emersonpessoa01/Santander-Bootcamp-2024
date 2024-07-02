fd95520cd058aa1f9d927c77c6770dbd2e59e315
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-07-02 11:52:43 -0300

    added aula-spring-data-jpa :rocket: User user = new User();         user.setName("GABRIEL NUNES");         user.setUsername("gabriel");         user.setPassword("santos");         repository.save(user);
            for(User u: repository.findAll()){
                System.out.println(u);
            }

---
+++ Springboot/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -0,0 +1,28 @@
+package dio.aula;
+
+
+import dio.aula.model.User;
+import dio.aula.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartApp implements CommandLineRunner {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setName("EMERSON PESSOA");
+        user.setUsername("emerson");
+        user.setPassword("pessoa");
+        repository.save(user);
+
+        for(User u: repository.findAll()){
+            System.out.println(u);
+        }
+    }
+
+}
