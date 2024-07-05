c3aab231ff9a7d4c0d7c62933573e7281fe69d4e
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-07-05 17:10:59 -0300

    Enabling security with spring :rocket:

---
+++ Springboot/dio-spring-securitty/src/main/java/dio/spring_securitty/DioSpringSecurittyApplication.java
@@ -0,0 +1,13 @@
+package dio.spring_securitty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringSecurittyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringSecurittyApplication.class, args);
+	}
+
+}
---
+++ Springboot/dio-spring-securitty/src/test/java/dio/spring_securitty/DioSpringSecurittyApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.spring_securitty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringSecurittyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
